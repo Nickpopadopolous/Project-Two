0c8610a105d5d1fd0183bbd9de9166cf55a83cdf
Shivam Parekh <shivam@knights.ucf.edu>
2017-02-11 11:56:26 -0500

    server update

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-var http = require('http');
+// packages
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// create the app
+
+var app = express();
+
+// port
 
 var PORT = process.env.PORT || 3000;
 
-function handleRequest(request, response) {
+// use bodyParser
 
-	response.end("we here" + request.url);
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 
+// method override
 
-var server = http.createServer(handleRequest);
+app.use(methodOverride("_method"));
 
-server.listen(PORT, function() {
+// routes
+
+var routes = require('./controllers/controller.js');
+
+app.use('/', routes);
+
+// listenter
+
+app.listen(PORT, function() {
 
 	console.log("server is working");
+
 });
